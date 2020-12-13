3d7e08bdc4631a31061dc5bf1cfbce0ecad7ea99
Neal Sidhwaney <nealsid@users.noreply.github.com>
2020-12-13 15:59:23 -0600

    Initial Django models

--- nanny/frontend/models.py
+++ nanny/frontend/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    mrn = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+
+class VaccineManufacturer(models.Model):
+    manufacturer_name = models.CharField(max_length=50)
+    vaccine_name = models.CharField(max_length=50)
+
+class VaccineShipment(models.Model):
+    vaccine = models.ForeignKey(VaccineManufacturer, on_delete=models.PROTECT)
+    receive_date = models.DateTimeField('shipment received')
+    doses = models.IntegerField()
+
+class VaccineDose(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.PROTECT)
+    date_given = models.DateTimeField('dose date')
--- nanny/nanny/settings.py
+++ nanny/nanny/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend.apps.FrontendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'nanny.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'vaccinedb.sqlite3',
     }
 }
 
